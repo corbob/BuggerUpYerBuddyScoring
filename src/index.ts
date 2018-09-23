5ed58d00ebc488f6b219aa04e2f1108a095e459c
corbob <corbobgithub@knoxy.ca>
2018-09-23 15:08:37 -0700

    message

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,102 @@
-let Players: Array<Array<Hands>> = [[]];
+let players: Array<Player> = [];
 const SUITS: Array<string> = ['♥','♠','♦','♣','☺'];
-let headerRow: HTMLTableRowElement; // = document.getElementById('header') as HTMLTableRowElement;
-let scoreTable: HTMLTableElement; // = document.getElementById('score') as HTMLTableElement;
+let headerRow: HTMLTableRowElement;
+let subHeaderRow: HTMLTableRowElement;
+let scoreTable: HTMLTableElement;
+let scoreRow: HTMLTableRowElement = document.createElement('tr');
 
 function LoadVariables() {
     headerRow = document.getElementById('header') as HTMLTableRowElement;
+    subHeaderRow = document.getElementById('subHeader') as HTMLTableRowElement;
     scoreTable = document.getElementById('score') as HTMLTableElement;
+    scoreRow.appendChild(newCell());
+    scoreRow.appendChild(newCell());
+    scoreRow.appendChild(newCell());
 }
 
 function DrawScorecard() {
     let direction: number = 1;
     let numCards: number = 1;
     let totalPlayers: number = parseInt((document.getElementById('numPlayers') as HTMLInputElement).value);
+    if (3 > totalPlayers || totalPlayers > 10) {
+        window.alert("What the firetruck dude!");
+        throw new Error("What the firetruck dude!");
+    }
     let maxHands: number = Math.floor(52 / totalPlayers);
     // Set header
     for (let i: number = 0 ; i < totalPlayers ; i++ ) {
-        Players[i] = [];
-        var player = document.createElement('th');
-        player.colSpan = 3;
-        player.innerText = 'Player ' + i;
-        headerRow.appendChild(player);
+        players[i] = new Player('Player ' + i);
+        var currentPlayer = document.createElement('th');
+        currentPlayer.colSpan = 2;
+        currentPlayer.innerText = players[i].getName();
+        headerRow.appendChild(currentPlayer);
+        subHeaderRow.appendChild(newCell('Bid'));
+        subHeaderRow.appendChild(newCell('Taken'));
+        var scoreCell = newCell('0', 'totals');
+        scoreCell.colSpan = 2;
+        scoreRow.appendChild(scoreCell);
     }
     for (let i: number = 0 ; i < (maxHands * 2) ; i++) {
         if(numCards > maxHands) {
             direction = -1;
             numCards--;
         }
-        var scoreRow = document.createElement('tr');
-        scoreRow.id = 'hand' + numCards + 1;
-        var handNum = document.createElement('td');
-        handNum.innerText = numCards.toString();
+        var handRow = document.createElement('tr');
+        handRow.id = 'hand' + numCards + 1;
+        handRow.appendChild(newCell(numCards.toString()));
         numCards += direction;
-        scoreRow.appendChild(handNum);
-        var suit = document.createElement('td');
-        suit.innerText = SUITS[(i) % 5];
-        suit.className = SUITS[(i) % 5];
-        scoreRow.appendChild(suit);
-        scoreRow.appendChild(newCell('totalBid',0 as any));
+        handRow.appendChild(newCell(SUITS[i % 5], SUITS[i % 5]));
+        handRow.appendChild(newCell(0 as any));
         for (let j: number = 0 ; j < totalPlayers ; j++ ) {
-            // var player = document.createElement('td');
-            // player.innerHTML = '&nbsp;';
-            Players[j][i] = {
-                tricksBid: newCell('tricksBid','bid'),
-                tricksTaken: newCell('tricksTaken','taken'),
-                score: newCell('score','score'),
-            }
-            scoreRow.appendChild(Players[j][i].tricksBid);
-            scoreRow.appendChild(Players[j][i].tricksTaken);
-            scoreRow.appendChild(Players[j][i].score);
+            handRow.appendChild(newCell('input', 'trickBid'));
+            handRow.appendChild(newCell('input', 'trickTaken'));
+            // player[j].getHand(i) = {
+            //     tricksBid: newCell('input','tricksBid'),
+            //     tricksTaken: newCell('input','tricksTaken'),
+            // }
+            // scoreRow.appendChild(Players[j][i].tricksBid);
+            // scoreRow.appendChild(Players[j][i].tricksTaken);
+            // scoreRow.appendChild(Players[j][i].score);
         }
-        scoreTable.appendChild(scoreRow);
+        scoreTable.appendChild(handRow);
     }
+    scoreTable.appendChild(scoreRow)
 }
 
-let newCell = (className?: string, contents?: string) => {
+let newCell = (contents?: string, className?: string) => {
     let temp: HTMLTableCellElement = document.createElement('td');
     if(className !== undefined) {
         temp.className = className;
     }
-    if(contents !== undefined) {
+    if(contents !== undefined && contents !== 'input') {
         temp.innerText = contents;
+    } else if (contents === 'input') {
+        let inputTemp: HTMLInputElement = document.createElement('input');
+        inputTemp.type = 'number';
+        temp.appendChild(inputTemp)
     } else {
         temp.innerHTML = '&nbsp;';
     }
     return temp;
 }
 
-interface Hands {
-    tricksBid: HTMLTableCellElement;
-    tricksTaken: HTMLTableCellElement;
-    score: HTMLTableCellElement;
+class Player {
+    private name: string;
+    private hands: Array<Hand>;
+    constructor(name: string) {
+        this.name = name;
+    };
+    getName(): string {
+        return this.name;
+    }
+    getHand(handNum: number): Hand {
+        return this.hands[handNum];
+    };
+    setHand(handNum: number, hand: Hand) {
+
+    };
+}
+interface Hand {
+    tricksBid: number;
+    tricksTaken: number;
 }
